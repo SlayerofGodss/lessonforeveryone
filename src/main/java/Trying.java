58b751afbf6f7777f02b77f36e76d0293c3a2306
DİYAR <diyarbjk44@gmail.coo>
2020-05-05 15:34:58 +0300

    ufak hesaplamalar

---
+++ src/main/java/Trying.java
@@ -0,0 +1,12 @@
+public class Trying {
+    public static void main(String[] args) {
+
+        int a=10,b=20,c=40,toplam,cikan,bolum;
+        toplam=(a+b-c);
+        cikan=(c-a-b);
+        bolum=(c/b+2*a+c/b);
+        System.out.println("Toplam"+toplam);
+        System.out.println("Çıkan"+cikan);
+        System.out.println("Bölüm"+bolum);
+    }
+}
