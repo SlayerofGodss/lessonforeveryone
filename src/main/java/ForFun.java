28625b068f724efe8f5290346b422f4eca932f9c
DİYAR <diyarbjk44@gmail.coo>
2020-05-05 15:06:28 +0300

    diziler

---
+++ src/main/java/ForFun.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ForFun {
+    public static void main(String[] args) {
+
+        int a [] = new int[5];
+        a[3]=8 ;
+        System.out.println(""+a[3]);
+        int b[] = {1,3,5,8,9};
+        String str[] = {"ali","veli","zenci","bilgi","kırk haramiler"};
+        System.out.println(str[2]+str[3]+str[4]);
+
+
+
+    }
+}
