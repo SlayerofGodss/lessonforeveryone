72abf43cd91f024999afff3572850b4b9cbc84ec
DİYAR <diyarbjk44@gmail.coo>
2020-05-05 15:35:40 +0300

    ufak hesaplamalar

--- src/main/java/ForFun.java
+++ src/main/java/ForFun.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class ForFun {
     public static void main(String[] args) {
 
-        int a [] = new int[5];
-        a[3]=8 ;
-        System.out.println(""+a[3]);
-        int b[] = {1,3,5,8,9};
-        String str[] = {"ali","veli","zenci","bilgi","kırk haramiler"};
-        System.out.println(str[2]+str[3]+str[4]);
+ int a=10,b=20,c=40,toplam,cikan,bolum;
+ toplam=(a+b-c);
+ cikan=(c-a-b);
+ bolum=(c/b+2*a+c/b);
+ System.out.println("Toplam"+toplam);
+ System.out.println("Çıkan"+cikan);
+ System.out.println("Bölüm"+bolum);
+
+
+
+
 
 
 
