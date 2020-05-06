705859c3e47faf08bd822e5248abc60120be7a85
DİYAR <diyarbjk44@gmail.coo>
2020-05-06 14:21:49 +0300

    karakter sayısı bul

---
+++ src/main/java/Udemy4.java
@@ -0,0 +1,7 @@
+public class Udemy4 {
+    public static void main(String[] args){
+        String mesaj="Allahım Sana Geliyorum";
+        System.out.println(mesaj);
+        System.out.println("Elaman Sayısı : "+ mesaj.length());
+    }
+}
