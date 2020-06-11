3030753d481b8d30a06fba976c0c35c503a0e223
DİYAR <diyarbjk44@gmail.coo>
2020-06-11 11:56:40 +0300

    say Hello JavaFX

--- src/test/java/JavaFxDenemeler/Test.java
+++ src/test/java/JavaFxDenemeler/Test.java
@@ -1,10 +1,15 @@
 package JavaFxDenemeler;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 public class Test extends Application        {
    @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("İlk Java Uygulaması");
+        Label label = new Label("Merhaba JavaFx");
+        Scene scene = new Scene(label,250,100);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
     public static void main(String[] args){
